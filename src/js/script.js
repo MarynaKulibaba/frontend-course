baacc18d05a831cf0a802fc20da5e7cb6fbed1e0
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-06 11:39:43 +0200

    hw 20 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let age = +prompt('Ваш вік?');
+let inRange = !(isNaN(age) || age > 130 || age <= 0);
+
+if (inRange) {
+    let suffix = 'років';
+
+    if (age % 100 < 11 || age % 100 > 14) {
+        if (age % 10 === 1) {
+            suffix = 'рік';
+        }
+        if (age % 10 >= 2 && age % 10 <= 4) {
+            suffix = 'роки';
+        }
+    }
+
+    alert(`${age} ${suffix}`);
+} else {
+    alert('Будь ласка, ведіть  число від 1 до 130');
+}
