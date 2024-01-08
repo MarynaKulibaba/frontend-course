821c337e2d20ecbb91d62c159c0a0224d3075486
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-08 11:56:01 +0200

    hw 3 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const isNumber = (value) => !isNaN(parseFloat(value)) && isFinite(value);
+
+let firstOperand = prompt('Enter first number');
+let secondOperand = isNumber(firstOperand) ? prompt('Enter second number') : null;
+
+if (isNumber(secondOperand) ) {
+    firstOperand = +firstOperand;
+    secondOperand = +secondOperand;
+
+    console.log(`${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}`);
+    console.log(`${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`);
+    console.log(`${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`);
+    console.log(`${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`);
+}
+
+
+
