eb152e90b3b87f1c1ac565e7455e3c7a0f737acc
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-09 18:35:15 +0200

    hw 24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+
+const userNumber = prompt('Enter number');
+
+function findMinDivisor(userInput) {
+    const number = parseInt(userInput, 10);
+    if (isNaN(number) || number < 1) {
+        console.log('NaN');
+        return;
+    }
+
+    if (number === 2 || number === 3) {
+        // 2 і 3 є простими числами
+        console.log(`Число ${number} є простим.`);
+        return;
+    }
+
+    let divisor = 2;
+    while (divisor <= number) {
+        if (number % divisor === 0) {
+            break;
+        }
+        divisor++;
+    }
+    if (divisor == number) {
+        console.log(`Число ${number} є простим.`);
+        return;
+    }
+    console.log(`Найменший дільник числа ${number}: ${divisor}`);
+}
+
+findMinDivisor(userNumber);
