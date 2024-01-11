7dccca9256c965d5189733672ac9910a7d3c6a0c
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-11 18:40:28 +0200

    hw 27 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const userInputNumber = prompt('Enter a number');
+const userInputPow = userInputNumber != null ? prompt('Enter pow') : null;
+
+function displayPowNumber(num, pow = 1) {
+    num = parseInt(num, 10);
+    pow = parseInt(pow, 10);
+    if (
+        typeof num !== 'number' ||
+        isNaN(num) ||
+        typeof pow !== 'number' ||
+        isNaN(pow)
+    ) {
+        return 'помилка';
+    }
+    return Math.pow(num, pow);
+
+}
+
+const resultPow = displayPowNumber(userInputNumber, userInputPow);
+alert(resultPow);
