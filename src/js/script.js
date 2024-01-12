ea2a10a53a98fb775da7219a16e0231434921781
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-12 15:40:04 +0200

    hw 4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//1
+const firstString = prompt('Введіть перший рядок');
+const secondString = prompt('Введіть другий рядок');
+const thirdString = prompt('Введіть третій рядок');
+
+console.log(firstString + ' ' + secondString + ' ' + thirdString);
+
+//2
+const number = 55555;
+let numToString = number.toString();
+let result = '';
+
+for (let i = 0; i < numToString.length; i++) {
+ 	result = result + numToString[i] + ' ';
+ }
+ console.log(result);
