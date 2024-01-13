3c1f28d218e4e71d274b9f27aa8228af6d45e4dd
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-13 14:40:45 +0200

    hw 29 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function isValidArray(arr) {
+    return Array.isArray(arr) && arr.length > 0 ? arr : null;
+}
+
+function getPositiveNumbers(array) {
+    const arrPositiveNum = [];
+
+    if (!isValidArray(array)) {
+        alert('Input arrays is empty');
+        return null;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 0) {
+            arrPositiveNum.push(array[i]);
+        }
+    }
+
+    return isValidArray(arrPositiveNum) ? arrPositiveNum : null;
+}
+console.log(getPositiveNumbers(arr) ?? 'Array is empty');
