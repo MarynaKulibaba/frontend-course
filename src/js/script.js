272dd7ce8a95d371e6ae1a232cbac63ddec2d3d8
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-17 15:20:11 +0200

    hw 31 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const min = 0;
+const max = 100;
+const shuffledNumbers = [];
+
+for (let i = min; i < max - min + 1; i++) {
+    shuffledNumbers.push(i);
+}
+
+let indexOfLastShuffledNumber = shuffledNumbers.length - 1;
+const getUniqueRandomNumbers = function (){
+    const randomIndex = Math.floor(Math.random() * indexOfLastShuffledNumber);
+    const random = shuffledNumbers[randomIndex];
+    [shuffledNumbers[indexOfLastShuffledNumber], shuffledNumbers[randomIndex]] = [
+        shuffledNumbers[randomIndex],
+        shuffledNumbers[indexOfLastShuffledNumber],
+    ];
+    indexOfLastShuffledNumber--;
+    return random;
+}
+
+for (let i = 0; i <= 100; i++) console.log(getUniqueRandomNumbers());
