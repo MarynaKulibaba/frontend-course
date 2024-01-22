73e214f77516e8b9cd4c1bfaad7037779afbba8d
maryna.kulibaba <kulibaba.marine@gmail.com>
2024-01-22 17:57:48 +0200

    hw 15 found average value

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const isValidNumber = function (value) {
+    return typeof value === 'number' && !isNaN(value);
+};
+
+const getNumberFromUserInput = function (promptMessage) {
+    const input = prompt(promptMessage);
+    const number = parseInt(input, 10);
+
+    if (!isValidNumber(number)) {
+        throw new Error('Invalid input. Please provide a valid number.');
+    }
+
+    return number;
+};
+
+try {
+    const first = getNumberFromUserInput('Enter a number');
+    const second = getNumberFromUserInput('Enter second');
+    const third = getNumberFromUserInput('Enter third');
+
+    const average = (first + second + third) / 3;
+    console.log(`The average is: ${average}`);
+} catch (e) {
+    console.error(e);
+}
