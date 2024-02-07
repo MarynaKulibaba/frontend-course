4f835f39e667be59d570d9d903bcc706c74fd672
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-02-07 16:35:19 +0200

    hw 40 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,20 @@
-const reduce = function (arr, callback, initialValue = 0) {
+const reduce = function (arr, callback, initialValue) {
+  if (!Array.isArray(arr) || arr.length <= 0)
+    throw new Error('Unexpected array')
+  if (typeof callback !== 'function') throw new Error('Unexpected callback')
+
   let accum = initialValue
-  for (let i = initialValue; i < arr.length; i += 1) {
+  let startIndex = 0
+  if (typeof initialValue === 'undefined') {
+    accum = arr[0]
+    startIndex = 1
+  }
+  for (let i = startIndex; i < arr.length; i += 1) {
     accum = callback(accum, arr[i])
   }
   return accum
 }
-console.log(reduce([1, 2, 3, 4, 5], (accum, elem) => accum + elem, 0)) // 15
+
+const array = [1, 2, 3, 4, 5]
+const reduceCallback = (accum, elem) => accum + elem
+console.log(reduce(array, reduceCallback, 0))
