28ae7de202bcb6d8ba6803bd07d700b3321e3845
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-02-16 15:46:39 +0200

    hw 44 completed

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const complexArray = [
+  [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+  [5, [6, [7, [8, 9]]]],
+  [[10, 11], 12, 13],
+]
+
+const flat = function (arr) {
+  if (arguments.length > 1) {
+    throw new Error(
+      'Function accepts only 1 argument, too much arguments provided'
+    )
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error('Not array')
+  }
+
+  const newArray = []
+  ;(function flatten(array) {
+    array.forEach((el) => (Array.isArray(el) ? flatten(el) : newArray.push(el)))
+  })(arr)
+  return newArray
+}
+
+console.log(flat(complexArray))
