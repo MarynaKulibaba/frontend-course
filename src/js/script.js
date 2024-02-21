a0b39fb48dcea598369756d174a2d0f364c57f93
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-02-21 15:34:15 +0200

    hw 48 completed

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// 1
+const sumNum = (a, b) => a + b
+console.log(sumNum(2, 3))
+
+// 2
+const strLength = (str) => str.length
+
+console.log(strLength('string'))
+
+// 3
+const incrementArrNum = (arr) => arr.map((num) => num + 1)
+
+console.log(incrementArrNum([1, 2, 3, 4, 5]))
