754abd7cc4083b036b7015da59d865cb93c39b8b
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-06 19:28:16 +0200

    hw 55 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,22 @@
-const createTable = (rows, cols) => {
-  const table = document.createElement('table')
+;(function () {
+  const createTable = (rows, cols) => {
+    const table = document.createElement('table')
 
-  let index = 1
-  for (let row = 0; row < rows; row += 1) {
-    const tr = document.createElement('tr')
-    for (let column = 0; column < cols; column += 1) {
-      const td = document.createElement('td')
-      td.textContent = index
-      tr.append(td)
-      index += 1
+    let index = 1
+    for (let row = 0; row < rows; row += 1) {
+      const tr = document.createElement('tr')
+      for (let column = 0; column < cols; column += 1) {
+        const td = document.createElement('td')
+        td.textContent = index
+        tr.append(td)
+        index += 1
+      }
+      table.append(tr)
     }
-    table.append(tr)
+
+    return table
   }
 
-  return table
-}
-
-const table = createTable(10, 10)
-document.body.append(table)
+  const table = createTable(10, 10)
+  document.body.append(table)
+})()
