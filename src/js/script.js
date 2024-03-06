c6bb1f30708ea6c7c3355af4113fd3dd52824c85
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-06 15:30:39 +0200

    hw 59 completed

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', () => {
+  const inputValue = document.querySelector('input').value
+  const span = document.createElement('span')
+  span.innerHTML = inputValue
+  document.querySelector('.block').prepend(span)
+  document.querySelector('input').value = ''
+})
