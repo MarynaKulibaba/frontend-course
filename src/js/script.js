c68b7cabb306f44561ee19b78c9bee83a51b49bb
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-06 19:31:15 +0200

    hw 59 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
-document.querySelector('button').addEventListener('click', () => {
-  const inputValue = document.querySelector('input').value
-  const span = document.createElement('span')
-  span.innerHTML = inputValue
-  document.querySelector('.block').prepend(span)
-  document.querySelector('input').value = ''
-})
+;(function () {
+  document.querySelector('button').addEventListener('click', () => {
+    const inputValue = document.querySelector('input').value
+    const span = document.createElement('span')
+    span.innerHTML = inputValue
+    document.querySelector('.block').prepend(span)
+    document.querySelector('input').value = ''
+  })
+})()
