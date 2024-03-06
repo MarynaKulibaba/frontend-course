5f5ad9a38283a104bb1cf4e1693c0682e5b96857
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-06 19:26:29 +0200

    hw 54 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,29 @@
-const generateList = (parentElement, array) => {
-  if (!Array.isArray(array)) {
-    throw new Error('not an array')
-  }
-
-  const ul = document.createElement('ul')
-  parentElement?.append(ul)
-
-  for (const element of array) {
-    const li = document.createElement('li')
-    ul.append(li)
-    if (!Array.isArray(element)) {
-      li.textContent = element
-      continue
+;(function () {
+  const generateList = (parentElement, array) => {
+    if (!Array.isArray(array)) {
+      throw new Error('not an array')
     }
-    generateList(li, element)
+
+    const ul = document.createElement('ul')
+    parentElement?.append(ul)
+
+    for (const element of array) {
+      const li = document.createElement('li')
+      ul.append(li)
+      if (!Array.isArray(element)) {
+        li.textContent = element
+        continue
+      }
+      generateList(li, element)
+    }
+    return ul
   }
-  return ul
-}
 
-let list = generateList(null, [1, 2, 3])
-console.log(list)
-document.body.append(list)
+  let list = generateList(null, [1, 2, 3])
+  console.log(list)
+  document.body.append(list)
 
-list = generateList(null, [1, 2, [1.1, 1.2, 1.3], 3, [3.3, 3.4], 4])
-console.log(list)
-document.body.append(list)
+  list = generateList(null, [1, 2, [1.1, 1.2, 1.3], 3, [3.3, 3.4], 4])
+  console.log(list)
+  document.body.append(list)
+})()
