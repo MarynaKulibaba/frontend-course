dedff73e4b4e4b7b075c6e7f0b23bdeb2853036e
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-08 15:23:18 +0200

    hw 58 completed

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+;(function () {
+  const checkNumber = () => {
+    const inputValue = parseInt(document.getElementById('input').value, 10)
+    if (!Number.isNaN(inputValue) {
+      const result = input % 2 === 0 ? 'Парне число' : 'Не парне число';
+      alert(result);
+    }
+ 	}
+  }
+})()
