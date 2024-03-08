5eb908c0c3a8fe934b9cb8d5e2ba4731cef4e6b4
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-08 21:22:50 +0200

    hw 58 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,14 @@
-;(function () {
-  const checkNumber = () => {
-    const inputValue = parseInt(document.getElementById('input').value, 10)
-    if (!Number.isNaN(inputValue) {
-      const result = input % 2 === 0 ? 'Парне число' : 'Не парне число';
-      alert(result);
+const isNumber = (value) => !isNaN(parseFloat(value)) && isFinite(value)
+
+const checkNumber = (input) => {
+  let message = ''
+  try {
+    message = 'Введіть число!'
+    if (isNumber(input.value)) {
+      message = +input.value % 2 === 0 ? 'Парне число' : 'Не парне число'
     }
- 	}
+  } catch (error) {
+    message = `Невірне значення: ${error.message}`
   }
-})()
+  alert(message)
+}
