67aff8c26254509e977bdd460ea70e7224c1d3d1
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-14 15:42:38 +0200

    hw 61 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,40 @@
+// add option
 const addOption = () => {
   const optionInputs = document.getElementById('optionInputs')
   const optionCount = optionInputs.children.length + 1
+
+  const div = document.createElement('div')
+  div.className = 'option-inputs'
+
+  const label = document.createElement('label')
+  label.id = `option${optionCount}`
+  label.textContent = `Варіант відповіді ${optionCount}:`
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.id = `option${optionCount}`
+  input.required = true
+
+  div.append(label)
+  div.append(input)
+  optionInputs.append(div)
 }
 
-document.querySelector('.add-btn-option').addEventListener('click', addOption)
+const surveyForm = document.getElementById('survey-form')
+const addButton = surveyForm.querySelector('.add-btn-option')
+addButton.addEventListener('click', addOption)
+
+// create survey
+const createSurvey = () => {
+  const optionInputs = document.getElementById('optionInputs')
+  const surveyNameInput = document.getElementById('surveyName')
+  if (
+    surveyNameInput.validity.valueMissing &&
+    optionInputs.children.length < 2
+  ) {
+    console.log('не заповнен імя та менше 2')
+  }
+}
+
+const createButton = surveyForm.querySelector('.create-btn-survey')
+createButton.addEventListener('click', createSurvey)
