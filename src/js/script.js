5d4d8d54e45dbdf84cc6e819942bbcd81540410e
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-14 14:48:16 +0200

    hw 60 completed

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const addOption = () => {
+  const optionInputs = document.getElementById('optionInputs')
+  const optionCount = optionInputs.children.length + 1
+}
+
+document.querySelector('.add-btn-option').addEventListener('click', addOption)
