534d029c6edfe1f237c914c738bd914c62908fee
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-14 15:06:54 +0200

    hw 60 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
-;(function () {
-  document.querySelector('button').addEventListener('click', (event) => {
+;(() => {
+  const form = document.querySelector('#login-form')
+  const button = form.querySelector('button[type=submit]')
+  const message = document.querySelector('.message')
+  button.addEventListener('click', (event) => {
     event.preventDefault()
-    let message = ''
-    const form = document.querySelector('form')
-    message = !form.checkValidity()
-      ? 'Будь ласка, заповніть усі поля'
-      : (message = 'Реєстрація успішна')
-    form.append(message)
+    if (form.checkValidity()) {
+      message.classList.remove('error')
+      message.classList.add('success')
+      message.innerText = 'Реєстрація успішна'
+    } else {
+      message.classList.remove('success')
+      message.classList.add('error')
+      message.innerText = 'Будь ласка, заповніть усі поля'
+    }
   })
 })()
