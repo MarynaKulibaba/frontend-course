3923a05d44e935d6d77815b51ea411a654c93fe8
maryna.kulibaba <kulibaba_mari@ukr.net>
2024-03-15 14:15:17 +0200

    hw 61 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,16 @@
+const surveyForm = document.getElementById('survey-form')
+const addButton = surveyForm.querySelector('.add-btn-option')
+const createButton = surveyForm.querySelector('.create-btn-survey')
+const optionInputs = document.getElementById('optionInputs')
+const surveyNameInput = document.getElementById('surveyName')
+const message = document.querySelector('.message')
+const surveyResult = document.getElementById('surveyResult')
+const surveyNameResult = document.getElementById('surveyNameResult')
+const optionsResult = document.getElementById('optionsResult')
+const arrInput = () => Array.from(optionInputs.getElementsByTagName('input'))
+
 // add option
 const addOption = () => {
-  const optionInputs = document.getElementById('optionInputs')
   const optionCount = optionInputs.children.length + 1
 
   const div = document.createElement('div')
@@ -19,39 +29,40 @@ const addOption = () => {
   div.append(input)
   optionInputs.append(div)
 }
-
-const surveyForm = document.getElementById('survey-form')
-const addButton = surveyForm.querySelector('.add-btn-option')
 addButton.addEventListener('click', addOption)
 
 // create survey
-const createSurvey = () => {
-  const optionInputs = document.getElementById('optionInputs')
-  const surveyNameInput = document.getElementById('surveyName')
-  const message = document.querySelector('.message')
-  if (surveyNameInput.validity.valueMissing) {
-    message.classList.remove('success')
-    message.classList.add('error')
-    message.innerText = 'Будь ласка, введіть назву опитування'
-  } else if (optionInputs.children.length < 2) {
-    message.classList.remove('success')
-    message.classList.add('error')
-    message.innerText = 'Додайте принаймні два варіанти відповідей'
-  } else {
-    const surveyResult = document.getElementById('surveyResult')
-    surveyResult.style.display = 'block'
-    const surveyNameResult = document.getElementById('surveyNameResult')
-    const optionsResult = document.getElementById('optionsResult')
-    surveyNameResult.textContent = `Питання: ${surveyForm.elements[0].value}`
+const renderSurveyResult = () => {
+  surveyForm.style.display = 'none'
+  surveyResult.style.display = 'block'
+  surveyNameResult.textContent = `Питання: ${surveyForm.elements[0].value}`
 
-    const arrInput = Array.from(optionInputs.getElementsByTagName('input'))
-    arrInput.forEach((item) => {
-      const li = document.createElement('li')
-      li.textContent = item.value
-      optionsResult.append(li)
-    })
-  }
+  arrInput().forEach((item) => {
+    const li = document.createElement('li')
+    li.textContent = item.value
+    optionsResult.append(li)
+  })
 }
 
-const createButton = surveyForm.querySelector('.create-btn-survey')
+const createSurvey = () => {
+  try {
+    if (surveyNameInput.validity.valueMissing) {
+      throw new Error('Будь ласка, введіть назву опитування')
+    }
+    if (optionInputs.children.length < 2) {
+      throw new Error('Додайте принаймні два варіанти відповідей')
+    }
+    if (arrInput().some((e) => e.value.length === 0)) {
+      throw new Error('Заповніть варіанти відповідей')
+    }
+  } catch (e) {
+    message.classList.remove('success')
+    message.classList.add('error')
+    message.innerText = e.message
+    return
+  }
+  renderSurveyResult()
+}
 createButton.addEventListener('click', createSurvey)
+
+addOption()
